379b1361b98c63c1509036bac41b3472ec11fe16
amarin <17020181+asiripanich@users.noreply.github.com>
2020-11-18 17:57:44 +1100

    fix test

--- tests/testthat/test-register.R
+++ tests/testthat/test-register.R
@@ -1,5 +1,4 @@
 test_that("aurinapi_register works", {
   skip_if(!isTRUE(as.logical(Sys.getenv("CI"))))
-  temp_dir = tempdir()
-  aurinapi_register(username = "username", password = "password", save_dir = temp_dir)
+  aurinapi_register(username = "username", password = "password", add_to_renviron = T)
 })
