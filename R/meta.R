ba87f051c3747827274099046df896088049b78c
amarin <17020181+asiripanich@users.noreply.github.com>
2020-11-18 19:25:18 +1100

    rename meta to 'name' col to 'aurin_open_api_id'

--- R/meta.R
+++ R/meta.R
@@ -3,8 +3,7 @@
 #' @param force Logical. Default is `FALSE`. Loading metadata may takes a while,
 #'  so if it has been loaded before it will not load again unluss `force` is set
 #'  to `TRUE`.
-#' @return a data.frame with two columns -- name and title. The name column is
-#'  'AURIN Open API id' and can be used in `aurinapi_get()`.
+#' @return a data.frame with two columns -- aurin_open_api_id and title.
 #' @export
 #'
 #' @examples
@@ -16,7 +15,9 @@
 #'
 #' }
 aurinapi_meta = function(force = FALSE) {
-  wfs_client = get_aurinapi_wfs_client()
+  wfs_client = create_aurinapi_wfs_client()
   cli::cli_alert_info("Fetching available datasets...")
-  wfs_client$getFeatureTypes(pretty = TRUE)
+  meta = wfs_client$getFeatureTypes(pretty = TRUE)
+  names(meta)[names(meta) == "name"] = "aurin_open_api_id"
+  return(meta)
 }
