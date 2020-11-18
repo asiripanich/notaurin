bb3338f48cce96cfcd8cd6463567c0c3cb1966ef
amarin <17020181+asiripanich@users.noreply.github.com>
2020-11-19 01:25:17 +1100

    updated api key error msg

--- R/utils.R
+++ R/utils.R
@@ -4,7 +4,8 @@ has_aurin_api_userpwd = function() {
 
 stop_if_no_aurin_api_userpwd = function() {
   if (!has_aurin_api_userpwd()) {
-    stop("Has no Sys.getenv('AURIN_API_USERPWD'). Use `aurinapi_register()` to set it up.")
+    stop("Sys.getenv('AURIN_API_USERPWD') has not been set. ",
+         "Please use `aurinapi_register()` to save your AURIN API key.")
   }
   return(invisible(NULL))
 }
