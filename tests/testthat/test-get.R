ea40a8d94752167016636f62e4d06a71380d49c2
amarin <17020181+asiripanich@users.noreply.github.com>
2020-11-19 10:46:05 +1100

    prepare pkg for CRAN submission

--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -1,5 +1,6 @@
 test_that("aurinapi_get works", {
   skip_on_ci()
+  skip_on_cran()
   open_api_id = "aurin:datasource-au_govt_dss-UoM_AURIN_national_public_toilets_2017"
   data_sf = aurinapi_get(open_api_id)
   checkmate::expect_class(data_sf, classes = "sf")
