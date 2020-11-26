cf1dc15b580561c93ea9ceecd5da9fa3b33e0610
amarin <17020181+asiripanich@users.noreply.github.com>
2020-11-26 16:38:41 +1100

    move .onLoad to the pkg script

--- R/aurinapi-package.R
+++ R/aurinapi-package.R
@@ -6,3 +6,22 @@
 ## usethis namespace: start
 ## usethis namespace: end
 NULL
+
+client_wrapper = R6::R6Class(classname = "client_wrapper",
+                             public = list(
+                               client = NULL
+                             ))
+
+#' @export aurinapi_wfs_client_wrapper
+NULL
+
+.onLoad <- function(...){
+
+  if (Sys.getenv("AURIN_API_USERPWD") != "") {
+    assign("aurinapi_wfs_client_wrapper",
+           client_wrapper$new(),
+           envir = parent.env(environment()))
+  }
+
+}
+
