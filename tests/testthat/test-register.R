2ffb53d57eae1eb7e95322216ebaa02f07694857
asiripanich <17020181+asiripanich@users.noreply.github.com>
2021-07-26 17:58:00 +1000

    - Test `aur_register()`.

--- tests/testthat/test-register.R
+++ tests/testthat/test-register.R
@@ -1,4 +1,15 @@
 test_that("aur_register works", {
-  skip_if(!isTRUE(as.logical(Sys.getenv("CI"))))
-  aur_register(username = "username", password = "password", add_to_renviron = FALSE)
+  orginal_key <- Sys.getenv("AURIN_API_USERPWD")
+  aur_register(
+    username = "username",
+    password = "password",
+    add_to_renviron = FALSE,
+    overwrite = FALSE
+  )
+  expect_equal(Sys.getenv("AURIN_API_USERPWD"), "username:password")
+  
+  if (orginal_key != "") {
+    Sys.setenv(AURIN_API_USERPWD = orginal_key)
+  }
+
 })
