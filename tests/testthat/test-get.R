341be465c11b6fa4cdd85c8f11263c1dc78a121d
amarin <17020181+asiripanich@users.noreply.github.com>
2021-12-17 15:32:15 +1100

    test

--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -1,5 +1,4 @@
 test_that("aur_get works", {
-  skip_on_ci()
   skip_on_cran()
   open_api_id <- "aurin:datasource-UQ_ERG-UoM_AURIN_DB_public_toilets"
   data_sf <- aur_get(open_api_id)
--- tests/testthat/test-register.R
+++ tests/testthat/test-register.R
@@ -1,5 +1,5 @@
 test_that("aur_register works", {
-  orginal_key <- Sys.getenv("AURIN_API_USERPWD")
+  # orginal_key <- Sys.getenv("AURIN_API_USERPWD")
   aur_register(
     username = "username",
     password = "password",
@@ -8,7 +8,7 @@ test_that("aur_register works", {
   )
   expect_equal(Sys.getenv("AURIN_API_USERPWD"), "username:password")
 
-  if (orginal_key != "") {
-    Sys.setenv(AURIN_API_USERPWD = orginal_key)
-  }
+  # if (orginal_key != "") {
+  #   Sys.setenv(AURIN_API_USERPWD = orginal_key)
+  # }
 })
