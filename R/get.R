b0c6566041fff3c7f4e9a1797d74bab9fea50611
asiripanich <17020181+asiripanich@users.noreply.github.com>
2022-01-27 10:25:20 +1100

    - `aur_get()` now accepts additional parameters. You can now query a specific feature if you know its ID using `params = list(featureID=ID)`. See https://docs.geoserver.org/latest/en/user/services/wfs/reference.html#getfeature for other available options.

--- R/get.R
+++ R/get.R
@@ -8,7 +8,10 @@
 #'  by browsing the [Data Catalogue](https://aurin.org.au/resources/aurin-apis/aurin-open-api-and-r/).
 #'  The layer name is found in the ‘AURIN Open API ID’ field in a dataset’s metadata.
 #' @param crs default as "EPSG:4326". The Coordinate Reference System you wish to use
-#' @param params a named list that will be used when constructing the WFS request.
+#' @param params a named list that contains additional parameters to be used when
+#'  constructing a WFS GetFeature request. This is useful when you know the ID of
+#'  a specific feature or the maximum number of features you want to query.
+#'  See examples [here](https://docs.geoserver.org/latest/en/user/services/wfs/reference.html#getfeature).
 #'
 #' @return an `sf::sf` object.
 #' @export
@@ -17,9 +20,15 @@
 #' # follow the example in https://aurin.org.au/resources/aurin-apis/aurin-open-api-and-r/
 #' \dontrun{
 #' aur_get("aurin:datasource-UQ_ERG-UoM_AURIN_DB_public_toilets")
+#' 
+#' # Get the first 10 features.
+#' aur_get(
+#'    open_api_id,
+#'    params = list(maxFeatures = 10)
+#'  )
 #' }
 aur_get <- function(open_api_id, crs = "EPSG:4326", params = NULL) {
-  request <- aur_build_request(open_api_id, crs = "EPSG:4326", params = NULL)
+  request <- aur_build_request(open_api_id, crs = "EPSG:4326", params = params)
 
   cli::cli_alert_info("Downloading '{open_api_id}'...")
   .data <- sf::read_sf(request)
@@ -38,22 +47,26 @@ aur_get <- function(open_api_id, crs = "EPSG:4326", params = NULL) {
 #' @rdname aur_get
 aur_build_request <- function(open_api_id, crs = "EPSG:4326", params = NULL, outputFormat = "application/json") {
   checkmate::assert_string(open_api_id)
-  checkmate::assert_list(params, types = "character", names = "unique", null.ok = TRUE)
+  checkmate::assert_list(params, types = c("character", "numeric"), names = "unique", null.ok = TRUE)
 
   stop_if_no_aurin_api_userpwd()
 
   wfs <- glue::glue("http://{Sys.getenv('AURIN_API_USERPWD')}@openapi.aurin.org.au/wfs")
   url <- httr::parse_url(wfs)
-  url$query <- list(
-    service = "wfs",
-    version = "1.0.0",
-    request = "GetFeature",
-    srsName = crs,
-    outputFormat = outputFormat,
-    typename = open_api_id
+
+  url$query <- append(
+    list(
+      service = "wfs",
+      version = "1.0.0",
+      request = "GetFeature",
+      srsName = crs,
+      outputFormat = outputFormat,
+      typename = open_api_id
+    ),
+    params
   )
 
   request <- httr::build_url(url)
-
+  
   return(request)
 }
--- tests/testthat/test-get.R
+++ tests/testthat/test-get.R
@@ -1,7 +1,24 @@
 test_that("aur_get works", {
   skip_on_cran()
+  
   open_api_id <- "aurin:datasource-UQ_ERG-UoM_AURIN_DB_public_toilets"
+  
   data_sf <- aur_get(open_api_id)
   checkmate::expect_class(data_sf, classes = "sf")
   expect_snapshot(data_sf)
+
+
+  data_first_10_sf <- aur_get(
+    open_api_id,
+    params = list(maxFeatures = 10)
+  )
+  checkmate::assert_data_frame(data_first_10_sf, nrow = 10)
+
+  data_feature_28_sf <- aur_get(
+    open_api_id,
+    params = list(featureID = 28)
+  )
+  expect_equal(data_feature_28_sf$ogc_fid, 28)
+
 })
+
