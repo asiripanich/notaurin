4c04185fdebbb4c51a86c29a71131e1b37f91db6
asiripanich <17020181+asiripanich@users.noreply.github.com>
2022-04-29 15:47:30 +1000

    - Warn users that the package is not affiliated with AURIN on load.

--- R/zzz.R
+++ R/zzz.R
@@ -18,4 +18,11 @@ NULL
 
 .onLoad <- function(...) {
   assign("aurinapi_wfs_client_wrapper", client_wrapper$new(), envir = parent.env(environment()))
+  cli::cli_alert_warning(
+    paste("The `notaurin` package is not affiliated with AURIN.",
+    "Any issues or feedback related to the `notaurin` package should be reported",
+    "to https://github.com/asiripanich/notaurin.",
+    "If you would like to see the official AURIN R API Guide please visit",
+    "https://aurin.org.au/resources/aurin-apis/aurin-open-api-and-r/")
+  )
 }
